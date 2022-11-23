c73c484a02ec6f10371ea6984a98250cfd37f416
Grozdanovic <git.grozdanovic@gmail.com>
2022-11-23 11:28:00 +1100

    Add validate for name input

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,26 @@
 window.onload = function () {
-const date = new Date();
+  const date = new Date();
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
-let currentDate = `${day}-${month}-${year}`;
-document.getElementById('date').innerHTML = `Date: ${currentDate}`; 
+  let currentDate = `${day}-${month}-${year}`;
+  document.getElementById("date").innerHTML = `Date: ${currentDate}`;
+};
+
+const nameInput = document.querySelector("#nameInput");
+const submit = document.querySelector("#submit");
+const errMsg = document.querySelector("#errMsg");
+
+function validateNamInput() {
+  if (nameInput.value.length < 8) {
+    errMsg.innerText = "Length should be longer than 8";
+    errMsg.style.color = "red";
+    myName2.focus();
+  } else {
+    errMsg.innerText = "";
+  }
 }
+
+submit.addEventListener("click", validateNamInput);
