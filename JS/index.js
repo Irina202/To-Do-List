d78c6c0e38574f62f7814dbb39f2667c196ef305
NanaYay-dev <nana000000000@gmail.com>
2022-11-29 09:55:42 +1100

    add classes

--- JS/index.js
+++ JS/index.js
@@ -13,6 +13,7 @@ window.onload = function () {
 
 const nameInput = document.querySelector("#nameInput");
 const submit = document.querySelector("#submit");
+const reset = document.getElementById("#reset");
 const nameErr = document.querySelector(".nameErr");
 const descErr = document.querySelector(".descErr");
 const descriptionInput = document.getElementById("descriptionInput");
@@ -37,7 +38,7 @@ function validateDescriptonInput() {
   } else {
     descErr.innerText = "";
   }
-  console.log(descriptionInput.value)
+
 }
 
 function isDueDateValid() {
@@ -80,26 +81,30 @@ getDate()
 
 
 submit.addEventListener("click", () => {
-
+  let taskName = nameInput.value;
+  let description = descriptionInput.value;
+  let dueData = dueDate.value;
   let assignName = assignTo.options[assignTo.selectedIndex].text;
   console.log(assignName)
   let optionSelectedText = taskStatus.options[taskStatus.selectedIndex].text;
   console.log(optionSelectedText)
 
-  let id = Math.random().toString(16).slice(2);
-  console.log(id)
+  // let id = Math.random().toString(16).slice(2);
+  // console.log(id)
 
 
 function addItem(item){
-  const itemHTML = '<div class="card" style="width: 25rem; margin-top: 5rem">\n' +
+  const itemHTML = '<div class="card">\n' +
   '<div class="card-body" id='+item.id+'>\n' +
    ' <h5 class="card-name"><strong>Task name:</strong>'+item.name+'</h5>\n' +
    ' <h6 class="card-date mb-2 text-muted">Due Date: '+item.dueDate+'</h6>\n' +
    '<p class="task-status output"><strong>Assign to : '+item.assign+'</strong></p>\n' +
+   '<div class="card-scroll">\n'+
     '<p class="card-description">\n' +
      ' <strong>Description:</strong>\n' +
      ' '+item.description+'\n' +
    ' </p>\n' +
+   '  </div>\n' +
    '<p class="task-status"><strong>Status : '+item.status+'</strong></p>\n' +
    '<button type="button" class="btn btn-outline-success">\n' +
    ' Done\n' +
@@ -115,12 +120,58 @@ function addItem(item){
 
 addItem({
    id : id,
-  'name': nameInput.value,
-  'description': descriptionInput.value,
-  'dueDate' : dueDate.value,
+  'name': taskName,
+  'description': description,
+  'dueDate' : dueData,
   'assign' : assignName ,
   'status' : optionSelectedText,
    })
 
-})
+
+
+// let toarray = [];
+// let addItemToArray = {
+//   id : id,
+//  'name': taskName,
+//  'description': description,
+//  'dueDate' : dueData,
+//  'assign' : assignName ,
+//  'status' : optionSelectedText,
+//   }
+
+//   toarray.push(addItemToArray)
+
+//   console.log(toarray)
+
+//  const addTasks = (task) => {
+//   const updatedTodoList = toDoList.map((todo) => {
+//      if(todo.id === task.id){
+//       todo.tasks.push(task);
+//      }
+//      return todo;
+//   })
+//   setToDoList(updatedTodoList);
+// }
+
+   // localStorage.setItem("todoarray", JSON.stringify(arr));
+    //let storedTodos = JSON.parse(localStorage.getItem("todoarray"));
+
+    //console.log(storedTodos)
+  })
+  // window.addEventListener("load", () => loadPage());
+  // function loadPage() {
+  //     function fetchData () {
+  //         fetch("https://jwd09-task-api.herokuapp.com")
+  //         .then(resp => resp.json())
+  //         .then(data => console.log(data))
+  //         .then(err => console.log(err))
+  //     }
+  //     fetchData()
+  // }
+
+
+
+
+
+
 
