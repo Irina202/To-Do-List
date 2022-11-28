1531d45546153569932fbb3c9a70589fbadab909
NanaYay-dev <nana000000000@gmail.com>
2022-11-28 11:21:07 +1100

    my version of classes

---
+++ JS/fetchData.js
@@ -0,0 +1,10 @@
+import TaskManager from './classes' ''//from modules.js
+import './classes'//from clases.js
+
+
+
+
+function fetchData(){
+//let newTask =  new TaskManager(eachobj.title)
+}
+
--- JS/index.js
+++ JS/index.js
@@ -27,15 +27,17 @@ function validateNameInput() {
   } else {
     nameErr.innerText = "";
   }
+  console.log(nameInput.value)
 }
 
 function validateDescriptonInput() {
-  if (nameInput.value.length < 15) {
+  if (descriptionInput.value.length < 15) {
     descErr.innerText = "Length should be longer than 15";
     descErr.style.color = "red";
   } else {
     descErr.innerText = "";
   }
+  console.log(descriptionInput.value)
 }
 
 function isDueDateValid() {
@@ -43,40 +45,82 @@ function isDueDateValid() {
   return !!dateRaw && new Date(dateRaw) > new Date();
 }
 
-function ddAssign() {
-  let optionSelectedText = assignTo.options[assignTo.selectedIndex].text;
-  console.log("This task assign to : " + optionSelectedText);
-}
-
-function ddStatus() {
-  let optionSelectedText = taskStatus.options[taskStatus.selectedIndex].text;
-  console.log("Status : " + optionSelectedText);
-}
-
 function inputPlaceholders() {
   nameInput.placeholder = "Type your name";
   descriptionInput.placeholder = "Type your description";
+  dueDate.placeholder = "MM/DD/YYY"
 }
 inputPlaceholders();
 
+function getDate () {
+  submit.addEventListener("click", () => {
+
+    const isNameValid = validateNameInput();
+    const isDescriptionValid = validateDescriptonInput();
+    const dueDateValid = isDueDateValid();
+
+    const dueDateErr = document.querySelector(".date-err");
+    if (!dueDateValid) {
+      dueDateErr.classList.remove("d-none");
+    } else {
+      dueDateErr.classList.add("d-none");
+    }
+
+    const formValid = isNameValid && isDescriptionValid && dueDateValid;
+
+    if (formValid) {
+      // Dodaj novu karticu
+    }
+    console.log(dueDate.value)
+  });
+}
+getDate()
+
+//Adding a class
+
+
 submit.addEventListener("click", () => {
-  ddAssign();
-  ddStatus();
 
-  const isNameValid = validateNameInput();
-  const isDescriptionValid = validateDescriptonInput();
-  const dueDateValid = isDueDateValid();
+  let assignName = assignTo.options[assignTo.selectedIndex].text;
+  console.log(assignName)
+  let optionSelectedText = taskStatus.options[taskStatus.selectedIndex].text;
+  console.log(optionSelectedText)
 
-  const dueDateErr = document.querySelector(".date-err");
-  if (!dueDateValid) {
-    dueDateErr.classList.remove("d-none");
-  } else {
-    dueDateErr.classList.add("d-none");
-  }
+  let id = Math.random().toString(16).slice(2);
+  console.log(id)
 
-  const formValid = isNameValid && isDescriptionValid && dueDateValid;
 
-  if (formValid) {
-    // Dodaj novu karticu
-  }
-});
+function addItem(item){
+  const itemHTML = '<div class="card" style="width: 25rem; margin-top: 5rem">\n' +
+  '<div class="card-body" id='+item.id+'>\n' +
+   ' <h5 class="card-name"><strong>Task name:</strong>'+item.name+'</h5>\n' +
+   ' <h6 class="card-date mb-2 text-muted">Due Date: '+item.dueDate+'</h6>\n' +
+   '<p class="task-status output"><strong>Assign to : '+item.assign+'</strong></p>\n' +
+    '<p class="card-description">\n' +
+     ' <strong>Description:</strong>\n' +
+     ' '+item.description+'\n' +
+   ' </p>\n' +
+   '<p class="task-status"><strong>Status : '+item.status+'</strong></p>\n' +
+   '<button type="button" class="btn btn-outline-success">\n' +
+   ' Done\n' +
+ ' </button>\n' +
+    '<button type="button" class="btn btn-outline-danger">\n' +
+     ' Delete\n' +
+   ' </button>\n' +
+'  </div>\n' +
+'</div>';
+  const itemsContainer = document.getElementById("additem");
+  itemsContainer.innerHTML += itemHTML;
+}
+
+addItem({
+   id : id,
+  'name': nameInput.value,
+  'description': descriptionInput.value,
+  'dueDate' : dueDate.value,
+  'assign' : assignName ,
+  'status' : optionSelectedText,
+   })
+
+})
+
